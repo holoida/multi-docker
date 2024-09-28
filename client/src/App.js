142ba1f66b34f4fd4cfc2f6129b1f63182834b85
VolodymyrHoloida <volodimirgoloida@Volodimirs-MacBook-Pro.local>
2024-09-28 16:34:16 +0200

    feat: adding the same thing as stephen + update memory to 256

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn Re 2act
+            Learn React
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
